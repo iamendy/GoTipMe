e9e162f937f9321ba71df42b0c8ee5aa66e080b5
Nnamdi <17215830+iamendy@users.noreply.github.com>
2023-06-14 17:49:49 +0100

    update: contract and tests

---
+++ contracts/GoFundMe.sol
@@ -0,0 +1,182 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract GoTipMe is ReentrancyGuard {
+  using Counters for Counters.Counter;
+  Counters.Counter counter;
+
+  struct Tip {
+    uint id;
+    address owner;
+    string title;
+    string description;
+    uint maxAmount;
+    bool isActive;
+    uint createdAt;
+  }
+
+  struct Payment {
+    uint tipId;
+    address owner;
+    address giver;
+    uint amount;
+  }
+
+  //for storing all tips created by a user
+  mapping(address => Tip[]) allUserTips;
+
+  //to get a single tip
+  mapping(address => mapping(uint => Tip)) tips;
+
+  //for storing all payments received from a Tip
+  mapping(uint => Payment[]) tipPayments;
+
+  event NewTipCreated(
+    uint id,
+    address indexed owner,
+    string title,
+    string description,
+    uint maxAmount,
+    bool isActive,
+    uint createdAt
+  );
+  event NewPaymentReceived(
+    uint tipId,
+    address indexed owner,
+    address indexed giver,
+    uint amount
+  );
+
+  //create a new tip
+  function createNewTip(
+    string memory _title,
+    string memory _description,
+    uint _maxAmount
+  ) external {
+    counter.increment();
+
+    Tip memory tip = Tip({
+      id: counter.current(),
+      owner: msg.sender,
+      title: _title,
+      description: _description,
+      maxAmount: _maxAmount,
+      isActive: true,
+      createdAt: block.timestamp
+    });
+
+    //save new tip template to caller
+    allUserTips[msg.sender].push(tip);
+    tips[msg.sender][counter.current()] = tip;
+
+    //broadcast event
+    emit NewTipCreated(
+      counter.current(),
+      msg.sender,
+      _title,
+      _description,
+      _maxAmount,
+      true,
+      tip.createdAt
+    );
+  }
+
+  //get all tips created by a user
+  function getUserTips(address user) external view returns (Tip[] memory) {
+    Tip[] memory userTips = new Tip[](allUserTips[user].length);
+    for (uint i = 0; i < allUserTips[user].length; i++) {
+      userTips[i] = allUserTips[user][i];
+    }
+
+    return userTips;
+  }
+
+  //get a single tip for making payments
+  function getTip(
+    address _user,
+    uint _tipId
+  ) external view returns (Tip memory) {
+    //check if Tip exists
+    Tip memory tip = tips[_user][_tipId];
+    require(tip.id > 0, "404. Not found");
+    return tip;
+  }
+
+  //to get all payments for an owner's Tip
+  function getTipPayments(
+    address _user,
+    uint _tipId
+  ) external view returns (Payment[] memory) {
+    Tip memory tip = tips[_user][_tipId];
+
+    //checks if owner is caller
+    require(tip.owner == msg.sender, "Not Owner");
+
+    Payment[] memory payments = new Payment[](tipPayments[_tipId].length);
+    for (uint i = 0; i < tipPayments[_tipId].length; i++) {
+      payments[i] = tipPayments[_tipId][i];
+    }
+
+    return payments;
+  }
+
+  //for giving tips
+  function giveTip(address _owner, uint _tipId) external payable {
+    Tip memory tip = tips[_owner][_tipId];
+    require(msg.value > 0, "Invalid amount");
+    require(
+      msg.value <= tip.maxAmount,
+      "Too Kind! Please send a lower amount."
+    );
+
+    tipPayments[_tipId].push(Payment(_tipId, tip.owner, msg.sender, msg.value));
+
+    emit NewPaymentReceived(_tipId, tip.owner, msg.sender, msg.value);
+  }
+
+  //get total amount raised on a Tip
+  function getTipTotalAmount(
+    address _user,
+    uint _tipId
+  ) public view returns (uint) {
+    Tip memory tip = tips[_user][_tipId];
+
+    //checks if owner is caller
+    require(tip.owner == msg.sender, "Not Owner");
+
+    uint total = 0;
+    for (uint i = 0; i < tipPayments[_tipId].length; i++) {
+      total += tipPayments[_tipId][i].amount;
+    }
+    return total;
+  }
+
+  //withdraw funds and deactivate Tip
+  function withdrawMoney(address owner, uint _tipId) external {
+    Tip memory tip = tips[owner][_tipId];
+
+    //checks if tip is active
+    require(tip.isActive == true, "Tips is already withdrawn");
+    //checks if caller is owner
+    require(tip.owner == msg.sender, "404. Not Authorized");
+
+    (bool success, ) = payable(msg.sender).call{
+      value: getTipTotalAmount(msg.sender, _tipId)
+    }("");
+    require(success, "Transfer failed.");
+
+    //deactivate Tip
+    allUserTips[tip.owner][_tipId - 1].isActive = false;
+  }
+}
+
+//const x = await GoTipMe.deployed()
+//await x.createNewTip("title","description","3000000000000000000")
+//await x.giveTip("0xCa1604B345ac7001e7F442676d02F0E22797118a", 1, {from: '0x9081e967D39587aaE9F218a8ea6E8671555d4161', value: '1000000000000000000'})
+//await x.giveTip("0xCa1604B345ac7001e7F442676d02F0E22797118a", 1, {from: '0x9081e967D39587aaE9F218a8ea6E8671555d4161', value: '2000000000000000000'})
+//await x.withdrawMoney(1)
+//await x.allUserTips('0xD7451cCE6e241a38eA38079Ab1Ae3A3EBB6B366a',1)
---
+++ test/GoFundMe.js
@@ -0,0 +1,120 @@
+const { ethers } = require("ethers");
+const { expect } = require("chai");
+
+// Import the compiled contract artifacts
+const GoTipMe = artifacts.require("GoTipMe");
+
+contract("GoTipMe", (accounts) => {
+  let goTipMe;
+  let owner;
+  let giver;
+
+  before(async () => {
+    // Deploy the contract
+    goTipMe = await GoTipMe.new();
+    // Get accounts from ethers provider
+    owner = accounts[0];
+    giver = accounts[1];
+    provider = new ethers.providers.JsonRpcProvider();
+  });
+
+  describe("createNewTip", () => {
+    it("should create a new tip", async () => {
+      const title = "My First Tip";
+      const description = "This is a test tip";
+      const maxAmount = ethers.utils.parseEther("1");
+
+      // Create a new tip
+      await goTipMe.createNewTip(title, description, maxAmount, {
+        from: owner,
+      });
+
+      // Get the user's tips
+      const userTips = await goTipMe.getUserTips(owner, {
+        from: owner,
+      });
+
+      console.log(userTips);
+
+      // Check if the tip was created successfully
+      expect(userTips.length).to.equal(1);
+      expect(userTips[0].title).to.equal(title);
+      expect(userTips[0].description).to.equal(description);
+      expect(ethers.utils.formatEther(userTips[0].maxAmount)).to.equal(
+        ethers.utils.formatEther(maxAmount)
+      );
+      expect(userTips[0].isActive).to.be.true;
+    });
+  });
+
+  describe("giveTip", () => {
+    it("should give a tip to a tip owner", async () => {
+      const title = "My First Tip";
+      const description = "This is a test tip";
+      const maxAmount = ethers.utils.parseEther("1");
+
+      // Create a new tip
+      await goTipMe.createNewTip(title, description, maxAmount, {
+        from: owner,
+      });
+
+      // Get the user's tips
+      const userTips = await goTipMe.getUserTips(owner, {
+        from: owner,
+      });
+
+      // Get the tip ID of the newly created tip
+      const tipId = userTips[0].id;
+
+      // Give a tip to the owner
+      const tipAmount = ethers.utils.parseEther("0.5");
+      await goTipMe.giveTip(owner, tipId, {
+        value: tipAmount,
+        from: giver,
+      });
+
+      // Get the tip payments for the tip owner
+      const tipPayments = await goTipMe.getTipPayments(owner, tipId, {
+        from: owner,
+      });
+
+      // Check if the payment was received successfully
+      expect(tipPayments.length).to.equal(1);
+      expect(tipPayments[0].owner).to.equal(owner);
+      expect(tipPayments[0].giver).to.equal(giver);
+      expect(ethers.utils.formatEther(tipPayments[0].amount)).to.equal(
+        ethers.utils.formatEther(tipAmount)
+      );
+    });
+  });
+
+  describe("withdrawMoney", () => {
+    it("should withdraw the total amount raised and deactivate the tip", async () => {
+      const title = "My First Tip";
+      const description = "This is a test tip";
+      const maxAmount = ethers.utils.parseEther("1");
+
+      // Create a new tip
+      await goTipMe.createNewTip(title, description, maxAmount, {
+        from: owner,
+      });
+
+      // Get the user's tips
+      const userTips = await goTipMe.getUserTips(owner);
+
+      // Get the tip ID of the newly created tip
+      const tipId = userTips[0].id;
+
+      // Give a tip to the owner
+      const tipAmount = ethers.utils.parseEther("0.5");
+      await goTipMe.giveTip(owner, tipId, { value: tipAmount, from: giver });
+
+      // Withdraw the money from the tip
+      await goTipMe.withdrawMoney(owner, tipId, { from: owner });
+
+      // Check if the tip is deactivated
+      const updatedUserTips = await goTipMe.getUserTips(owner, { from: owner });
+      expect(updatedUserTips[0].isActive).to.be.false;
+    });
+  });
+});
